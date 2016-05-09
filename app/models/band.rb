31b13b13a5d493dda3aafefc64f91cc0f1321f2f
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-09 17:20:18 -0400

    Fixed associations on band model

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,8 +2,8 @@ class Band < ActiveRecord::Base
   belongs_to :leader, class_name: 'Musician', foreign_key: :musician_id
   belongs_to :genre
   has_many :members
-  has_many :contracts, through: :members
+  has_many :musicians, through: :members
 
   accepts_nested_attributes_for :members, reject_if: :all_blank
-  validates_associated :genre, :musician
+  validates_associated :genre, :leader
 end
