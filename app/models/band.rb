f51d288269b847d0493220a629df7827def1a016
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-09 17:19:26 -0400

    Added musician_id to band and added band associations

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,9 @@
 class Band < ActiveRecord::Base
+  belongs_to :leader, class_name: 'Musician', foreign_key: :musician_id
+  belongs_to :genre
+  has_many :members
+  has_many :contracts, through: :members
+
+  accepts_nested_attributes_for :members, reject_if: :all_blank
+  validates_associated :genre, :musician
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509210226) do
+ActiveRecord::Schema.define(version: 20160509211626) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160509210226) do
     t.string   "genre_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "musician_id"
   end
 
   create_table "genres", force: :cascade do |t|
