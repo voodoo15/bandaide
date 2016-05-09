9b9b03523cf13b5fba544c3850aaf6a379a048d1
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-09 17:02:07 -0400

    Added band model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509205704) do
+ActiveRecord::Schema.define(version: 20160509210104) do
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "genre_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.integer  "band_id"
