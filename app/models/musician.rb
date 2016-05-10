cd37e7f922d4256cd53df680bb78ff8826198368
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-09 23:44:56 -0400

    Added associations to musician model and added additional fields to new registration page

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -7,4 +7,8 @@ class Musician < ActiveRecord::Base
   belongs_to :position
   has_many :bands
   has_many :members
+
+  validates :first_name, :last_name, :address, :city, :postal_code, presence:  true
+  validates_inclusion_of :province, in: %w(AB BC SK MB ON QC NB NS PE NL NT YT NU)
+  validates :postal_code, length: { is: 6 }
 end
