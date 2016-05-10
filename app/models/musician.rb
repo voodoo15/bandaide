a3b87e950c89fb0214ab0f629e2574750da7c3ea
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-09 23:30:56 -0400

    Added associations to musician model

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -3,4 +3,8 @@ class Musician < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :position
+  has_many :bands
+  has_many :members
 end
