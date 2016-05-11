71de9e8b18e03d9228a2997d06f073e89f659e6b
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-11 16:05:18 -0400

    Added default avatar images to musician and put some info on band show page for now

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
+  def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
 
