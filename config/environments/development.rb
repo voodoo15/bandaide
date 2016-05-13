f2a9cb42bcc3becd97939857bb2288f2c454f46c
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-13 13:45:06 -0400

    Added application.yml to gitignore, added env login/pw for development.rb email section

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            Rails.application.secrets.gmail_username,
-  password:             Rails.application.secrets.gmail_password,
+  user_name:            ENV["gmail_username"],
+  password:             ENV["gmail_password"],
   authentication:       :plain,
   enable_starttls_auto: true  }
 end
