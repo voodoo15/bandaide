b980dcf710c1466acd5d2a56e9175b4424109211
CALLUM BURNS <CBU787@GMAIL.COM>
2016-05-15 16:18:47 -0400

    Removed nearby musicians from musician show page; added nearby musicians to home page. Shows distance to musicians if logged in, or musician's city if not logged in. Not yet sorted by distance.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
     @musicians = Musician.all
     @bands = Band.all
     @genres = Genre.all
+    @musician = current_musician
+    if current_musician.present?
+      @nearby_musicians = @musician.nearbys(2, unit: :km)
+    end
   end
 end
--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,6 +1,6 @@
 class MusiciansController < ApplicationController
   def show
     @musician = Musician.find(params[:id])
-    @nearby_musicians = @musician.nearbys(5, unit: :km)
+    @nearby_musicians = @musician.nearbys(2, unit: :km)
   end
 end
