9ed2cf2633dcbe4ea518bf9fa080f4656413fab7
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-05-17 20:20:18 -0400

    moved googleapi script to show page

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,7 +4,6 @@ function initMap() {
    var myLatLng = {lat : map_placeholder.data('latitude'), lng : map_placeholder.data('longitude')};
 
 
-
   var locations = gon.nearby_musicians;
   console.log(locations)
   var map = new google.maps.Map(map_placeholder[0],{
@@ -12,18 +11,20 @@ function initMap() {
     zoom: 15
   });
 
+  var infowindow = new google.maps.InfoWindow({
+    content: "test"
+  });
 
     for (i = 0; i < locations.length; i++) {
       var location= new google.maps.LatLng(locations[i][0], locations[i][1]);
       var marker = new google.maps.Marker({
         position: location,
+        clickable:true,
+        url: '/',
+        title:"Hello World!"
       });
-      marker.setMap(map);
-    };
+      marker.addListener('click', function() {window.location.href = marker.url;});
+        marker.setMap(map);
+  };
+
 };
-  //
-  // = new google.maps.Marker({
-  //     position: myLatLng,
-  //     map: map,
-  //     title: "test"
-  //   });
