d90ab3072bf036d5a291757269287a071070b2fa
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-19 17:14:16 -0400

    Added youtube functionality to musician

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519163257) do
+ActiveRecord::Schema.define(version: 20160519210803) do
 
   create_table "approvals", force: :cascade do |t|
     t.integer "member_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160519163257) do
     t.float    "longitude"
     t.string   "avatar"
     t.text     "description"
+    t.string   "youtube_url"
   end
 
   add_index "musicians", ["email"], name: "index_musicians_on_email", unique: true
