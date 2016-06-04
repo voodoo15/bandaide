dcbe37f5e452b7b0d0da7892927a86a52e4ddcd2
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-06-04 11:09:07 -0400

    Removed localhost in production and added false to asset precompile

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Bandaide
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-config.assets.paths << "#{Rails.root}/app/assets/videos"
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -22,5 +22,8 @@ config.assets.paths << "#{Rails.root}/app/assets/videos"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
+
+
   end
 end
