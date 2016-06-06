7e0457f8c54d452d86804d3033e74066de47b89b
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-06-06 18:57:14 -0400

    Got the band and musician data to pop on the map and I can filter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,9 +11,9 @@ class SearchController < ApplicationController
 
     @nearby_bands = Band.near(@musician)
 
-    # gon.nearby_musicians = @nearby_musicians.collect{ |r| [r.id, r.firstname,  r.latitude, r.longitude, r.positions.where(skills: {mainskill: true}).first.description, "musicians", r.avatar.thumb.url]}
     gon.nearby_musicians = @nearby_musicians.collect{ |r| [r.id, r.firstname,  r.latitude, r.longitude, r.positions.where(skills: {mainskill: true}).first.description, "musicians", r.avatar.thumb.url]} +
                            @nearby_bands.collect{ |b| [b.id, b.name, b.latitude, b.longitude, b.genre.description, "bands", b.poster.thumb.url]}
+
     @positions=Position.all
     @genres= Genre.all
   end
