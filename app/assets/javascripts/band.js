43ac798de2acc3d5d2c985f08287840cd436590d
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-06-07 17:44:34 -0400

    fixed a fews things

--- app/assets/javascripts/band.js
+++ app/assets/javascripts/band.js
@@ -1,7 +1,7 @@
-$(document).on('ready page:load', function() {
-  $('.apply-button').on('click', function(e) {
-    e.preventDefault();
-
-    $(this).toggleClass('applied');
-  });
-});
+// $(document).on('ready page:load', function() {
+//   $('.apply-button').on('click', function(e) {
+//     e.preventDefault();
+//
+//     $(this).toggleClass('applied');
+//   });
+// });
